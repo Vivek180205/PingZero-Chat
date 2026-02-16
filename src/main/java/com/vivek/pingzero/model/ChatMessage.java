4bff6ad2ccb91f8a9019d8027b5a529d6f492a48
Vivek <vivekrathod180205@gmail.com>
2026-02-17 01:40:49 +0530

    Add chat history feature with DB persistence and timestamp ordering

--- src/main/java/com/vivek/pingzero/model/ChatMessage.java
+++ src/main/java/com/vivek/pingzero/model/ChatMessage.java
@@ -1,11 +1,19 @@
 package com.vivek.pingzero.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Data
+@Document(collection = "messages")
 public class ChatMessage {
+
+    @Id
+    private String id;
 
     private String sender;
     private String content;
-    private String type;
+    private LocalDateTime createdAt;
 }
